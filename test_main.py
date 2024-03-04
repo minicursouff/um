c94bd5b2e9b6fdc8824c422efc7ba65913ae86f6
minicursouff <162066672+minicursouff@users.noreply.github.com>
2024-03-03 21:40:08 -0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,1 +1,15 @@
+import pytest
+from main import Calc
 
+class TestCalc:
+  def setup_method(self):
+    self.inst = Calc()
+  def test_equal_1(self):
+    assert self.inst.equal(28, 96) == 124
+  def test_equal_2(self):
+    assert self.inst.equal(10, 100) == 110
+  def test_equal_3(self):
+    assert self.inst.equal_2(10, 100) == 1000
+  def test_equal_4(self):
+    assert self.inst.equal_2(10, 2) == 20
+  
